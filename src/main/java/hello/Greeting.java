ff9b1f1f76e20d7bd8b6c10f9745731ee59c09c1
Michael <littlebass09@tamu.edu>
2014-10-23 17:31:24 -0500

    MB initial commit, spring is working, this is the dummy commit of spring

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final int _age;
+
+    public Greeting(long id, String content, int age) {
+        this.id = id;
+        this.content = content;
+        _age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getAge() {return _age;}
+}
