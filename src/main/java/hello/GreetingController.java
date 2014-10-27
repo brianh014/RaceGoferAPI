0534ec88412408d74df20eacf4aab1afd4333dfd
Michael <littlebass09@tamu.edu>
2014-10-26 20:50:51 -0500

    Code for Tech Demo version 1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.sql.*;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,11 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    Connection connect = null;
+    Statement statement = null;
+    PreparedStatement preparedStatement = null;
+    int resultInt;
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
                              @RequestParam(value="age", required=false, defaultValue="0") String ageString) {
@@ -18,4 +24,27 @@ public class GreetingController {
                             String.format(template, name),
                             Integer.parseInt(ageString));
     }
+
+    @RequestMapping("/AddMyCoordinates")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
+                           @RequestParam(value="latitude", required=false, defaultValue="0") String latitude,
+                           @RequestParam(value="longitude", required=false, defaultValue="0") String longitude) {
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://productiondb.covqdeip5d2j.us-west-2.rds.amazonaws.com/RaceGofer?user=admin&password=howdyteam1");
+            statement = connect.createStatement();
+            resultInt = statement.executeUpdate("INSERT INTO DemoTable (Name, Latitude, Longitude) VALUES (\"" + name + "\"," + latitude + "," + longitude + ");");
+        } catch (SQLException e) {
+            System.out.println("ERROR 1");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("ERROR 2");
+            e.printStackTrace();
+        }
+
+        return "Your coordinates have been saved.";
+    }
+
 }
