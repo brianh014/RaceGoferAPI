579e519622a37a35006df342d5a5ec4b29e48d62
Miles Hickman <mileshick@gmail.com>
2014-11-13 15:34:04 -0600

    Need to test but db auth might be working?

--- src/main/java/com/racegofer/api/Application.java
+++ src/main/java/com/racegofer/api/Application.java
@@ -1,18 +1,45 @@
 package com.racegofer.api;
 
 //import com.racegofer.api.config.DatabaseConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+
+    @Value("${spring.datasource.driverClassName}")
+    private String databaseDriverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username}")
+    private String databaseUsername;
+
+    @Value("${spring.datasource.password}")
+    private String databasePassword;
+
+    @Bean
+    public DataSource datasource() {
+        org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
+        ds.setDriverClassName(databaseDriverClassName);
+        ds.setUrl(datasourceUrl);
+        ds.setUsername(databaseUsername);
+        ds.setPassword(databasePassword);
+
+        return ds;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
