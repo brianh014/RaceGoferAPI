4d83eba7c15e4ade49c23d75b718aa2f44ee2c65
Michael <littlebass09@tamu.edu>
2014-11-18 18:20:21 -0600

    Small update

--- src/main/java/com/racegofer/api/web/API_JoinRace.java
+++ src/main/java/com/racegofer/api/web/API_JoinRace.java
@@ -1,8 +1,13 @@
 package com.racegofer.api.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
 
 /**
  * Created by Michael on 11/8/2014.
@@ -11,6 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class API_JoinRace {
 
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource)
+    {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @RequestMapping("/JoinRace")
     public String JoinRace(
             @RequestParam(value="userName", required=true) String userName,
@@ -18,14 +31,12 @@ public class API_JoinRace {
             @RequestParam(value="password", required=true) String password,
             @RequestParam(value="userType", required=true) String userType)
     {
-        System.out.println(1);
-        Query query = new Query();
-        System.out.println(2);
+        //Query query = new Query();
         String queryString = "INSERT INTO `RaceGofer`.`UserRunsRace` (`RaceID`, `UserID`, `Type`) VALUES ('"
                 + raceId + "', '" + userName + "', '" + userType + "');";
-        System.out.println(3);
-        int result = query.ExecuteUpdate(queryString);
-        System.out.println(4);
+
+        //int result = query.ExecuteUpdate(queryString);
+        int result = jdbcTemplate.update(queryString);
         return "Race was Joined"; //only returns positive, needs to be edited
     }
 }
