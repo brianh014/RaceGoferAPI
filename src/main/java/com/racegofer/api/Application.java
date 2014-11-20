c37002bc1cc77c912dd788806e093b5198bbd451
Miles Hickman <mileshick@gmail.com>
2014-11-19 22:46:21 -0600

    Fixed the broken thigns

--- src/main/java/com/racegofer/api/Application.java
+++ src/main/java/com/racegofer/api/Application.java
@@ -4,6 +4,7 @@ package com.racegofer.api;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan
-@EnableJpaRepositories
 @EnableAutoConfiguration
+@ComponentScan
+@EntityScan
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/racegofer/api/domain/UserRepository.java
+++ src/main/java/com/racegofer/api/domain/UserRepository.java
@@ -2,11 +2,13 @@ package com.racegofer.api.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Miles on 11/9/2014.
  */
 
+@Repository
 public interface UserRepository extends CrudRepository<RaceUser, Long> {
     RaceUser findByUserName(@Param("userName") String userName);
 }
